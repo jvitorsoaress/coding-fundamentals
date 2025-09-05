b33bcbe002e3471bf8a110157142ca9ebc544e3b
jvitorsoaress <vitor.dev74@gmail.com>
2025-09-05 20:39:39 -0300

    Project adjustments

--- Programming/C#/Introduction/HelloWorld.cs
+++ Programming/C#/Introduction/HelloWorld.cs
@@ -1,11 +1,21 @@
 using System;
 
+/// <summary>
+/// In this class, I'll be focusing on the Logical and Basic C# Programming stuffs
+/// </summary>
 public class HelloWorld
 {
     public static int Main() //note: change the method signature so you can run this
     {
+        // So it all begins here:
+        // Basically there is 2 ways to print on the Console
         Console.WriteLine("Hello, World!");
+        Console.Write("Hello, ");
+        Console.Write("World!"); //The difference is that .Write() doesn't break the line
 
+         
+
+        
         return 0;
     }
 }
